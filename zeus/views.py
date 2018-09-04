7b7be7c11e6a35fad33438949f837fce83d5e0bd
flyliufu <flyliufu@sina.com>
2018-09-04 11:00:00 +0800

    add token

--- zeus/views.py
+++ zeus/views.py
@@ -27,9 +27,9 @@ def token(request):
         # 把参数放到list中排序后合成一个字符串，再用sha1加密得到新的字符串与微信发来的signature对比，如果相同就返回echostr给服务器，校验通过
         hashlist = [token, timestamp, nonce]
         hashlist.sort()
-        hashstr = ''.join([s for s in hashlist])
-        hashstr = hashlib.sha1(hashstr).hexdigest()
-        if hashstr == signature:
+        hash_str = ''.join([s for s in hashlist])
+        hash_str = hashlib.sha1(hash_str.encode()).hexdigest()
+        if hash_str == signature:
             return HttpResponse(echostr)
         else:
             return HttpResponse("field")
