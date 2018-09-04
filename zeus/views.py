7b956b23e3843409d7f46651fa49cd8570a782c1
flyliufu <flyliufu@sina.com>
2018-09-04 16:41:55 +0800

    bug fix

--- zeus/views.py
+++ zeus/views.py
@@ -16,46 +16,47 @@ logger = logging.getLogger("django.request")
 # django默认开启csrf防护，这里使用@csrf_exempt去掉防护
 @csrf_exempt
 def token(request):
-    if request.method == "GET":
-        # 接收微信服务器get请求发过来的参数
-        signature = request.GET.get('signature', None)
-        timestamp = request.GET.get('timestamp', None)
-        nonce = request.GET.get('nonce', None)
-        echostr = request.GET.get('echostr', None)
-        # 服务器配置中的token
-        token = 'weixin'
-        # 把参数放到list中排序后合成一个字符串，再用sha1加密得到新的字符串与微信发来的signature对比，如果相同就返回echostr给服务器，校验通过
-        hashlist = [token, timestamp, nonce]
-        hashlist.sort()
-        hash_str = ''.join([s for s in hashlist])
-        hash_str = hashlib.sha1(hash_str.encode()).hexdigest()
-        if hash_str == signature:
-            return HttpResponse(echostr)
+    try:
+        if request.method == "GET":
+            # 接收微信服务器get请求发过来的参数
+            signature = request.GET.get('signature', None)
+            timestamp = request.GET.get('timestamp', None)
+            nonce = request.GET.get('nonce', None)
+            echostr = request.GET.get('echostr', None)
+            # 服务器配置中的token
+            token = 'weixin'
+            # 把参数放到list中排序后合成一个字符串，再用sha1加密得到新的字符串与微信发来的signature对比，如果相同就返回echostr给服务器，校验通过
+            hashlist = [token, timestamp, nonce]
+            hashlist.sort()
+            hash_str = ''.join([s for s in hashlist])
+            hash_str = hashlib.sha1(hash_str.encode()).hexdigest()
+            if hash_str == signature:
+                return HttpResponse(echostr)
+            else:
+                return HttpResponse("field")
         else:
-            return HttpResponse("field")
-    else:
-        other_content = auto_reply(request)
-        logger.debug(
-            '''
-            =========RESPONSE========
-            %s
-            =========================
-            ''' % other_content
-        )
-        return HttpResponse(other_content)
+            logger.debug(
+                '''
+                =========REQUEST=========
+                %s
+                =========================
+                ''' % request.body.decode("utf-8")
+            )
+            other_content = auto_reply(request.body)
+            logger.debug(
+                '''
+                =========RESPONSE========
+                %s
+                =========================
+                ''' % other_content
+            )
+            return HttpResponse(other_content)
+    except Exception as e:
+        logger.error(str(e))
 
 
-def auto_reply(request):
-    webData = request.body
-    logger.debug(
-        '''
-        =========REQUEST=========
-        %s
-        =========================
-        ''' % webData.decode("utf-8")
-    )
+def auto_reply(webData):
     xml_data = ET.fromstring(webData)
-
     msg_type = xml_data.find('MsgType').text
     ToUserName = xml_data.find('ToUserName').text
     FromUserName = xml_data.find('FromUserName').text
