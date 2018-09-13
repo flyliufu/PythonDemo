7e20fe56a209031c12c4df237bdaa8e95a9eb3ce
flyliufu <flyliufu@sina.com>
2018-09-13 22:05:12 +0800

    add other msg time condition.

--- zeus/controller/BusinessController.py
+++ zeus/controller/BusinessController.py
@@ -2,18 +2,23 @@ from zeus.util.MsgUtil import MsgUtil
 
 # 微信服务器推送消息是xml的，根据利用ElementTree来解析出的不同xml内容返回不同的回复信息，就实现了基本的自动回复功能了，也可以按照需求用其他的XML解析方法
 import xml.etree.ElementTree as et
+import logging
+
+logger = logging.getLogger("django.request")
 
 
 class BusinessController:
 
     def auto_reply(self, body):
+        logging.debug(body)
+
         xml_data = et.fromstring(body)
         msg_type = xml_data.find('MsgType').text
         ToUserName = xml_data.find('ToUserName').text
         FromUserName = xml_data.find('FromUserName').text
         CreateTime = xml_data.find('CreateTime').text
         MsgType = xml_data.find('MsgType').text
-        MsgId = xml_data.find('MsgId').text
+        # MsgId = xml_data.find('MsgId').text
 
         content = "您好,欢迎来到Python学习!"
         if msg_type == 'text':
